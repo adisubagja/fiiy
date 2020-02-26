0ceb36ad4f1a74a451ccaf6cc06b974d4ba697ab
adisubagja <adisubagjaa@gmail.com>
2020-02-26 10:12:22 +0700

    Change text

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -236,8 +236,8 @@ async def kang(args):
                 # Ensure user doesn't get spamming notifications
                 await bot.send_read_acknowledge(conv.chat_id)
 
-        await args.edit(f"`Sticker kanged successfully!`\
-            \nPack can be found [here](t.me/addstickers/{packname})",
+        await args.edit(f"`Sticker Lu gue curry!`\
+            \nPack colongan di [mari](t.me/addstickers/{packname})",
                         parse_mode='md')
 
 
