d7c8d515badb1d0759fe55c1dfc64bc6a0c8c6c1
adisubagja <adisubagjaa@gmail.com>
2020-02-26 10:05:54 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -19,16 +19,16 @@ from telethon.tl.types import InputStickerSetID
 from telethon.tl.types import DocumentAttributeSticker
 
 KANGING_STR = [
-    "Using Witchery to kang this sticker...",
-    "Plagiarising hehe...",
-    "Inviting this sticker over to my pack...",
-    "Kanging this sticker...",
-    "Hey that's a nice sticker!\nMind if I kang?!..",
-    "hehe me stel ur stikér\nhehe.",
-    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
-    "Roses are red violets are blue, kanging this sticker so my pacc looks cool",
-    "Imprisoning this sticker...",
-    "Mr.Steal Your Sticker is stealing this sticker... ",
+    "Bentar nyeduh Kopi Dulu...",
+    "Mikir dulu hehe...",
+    "Masukin Sticker di my pack...",
+    "Ngekang Ni Sticker...",
+    "Hey Ini Sticker Bagus!\nGimana Jika Aku kang?!..",
+    "hehe Bentar Bentar bakar udut\nhehe.",
+    "Ay Coba liat (☉｡☉)!→\nApa Luu...",
+    "Bapac Toing ke Mekkah, Di mekkah eh masuk angin, Bisakah ini gw kangin?!",
+    "Compile This Sticker \nHehe...",
+    "Mr.Cabul Sticker mu bagus ni, minta yakk... ",
 ]
 
 
