6edf83ceca1ebbf3f2a2b5212a6037999d7c2838
adisubagja <adisubagjaa@gmail.com>
2020-02-27 13:17:54 +0700

    ahhem

--- userbot/modules/fakeload.py
+++ userbot/modules/fakeload.py
@@ -8,7 +8,7 @@ from time import sleep
 from userbot import CMD_HELP, ZALG_LIST
 from userbot.events import register
 
-@register(outgoing=True, pattern="^.fl(?: |$)(.*)")
+@register(outgoing=True, pattern="^.tg(?: |$)(.*)")
 async def typewriter(typew):
 	message = typew.pattern_match.group(1)
 	await typew.edit("`start loading tunggu sebentar!...`")
